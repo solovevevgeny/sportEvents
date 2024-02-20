0a6dfd9aa1bc1d290bac2d09676ab6f8cf5314f6
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-02-20 15:39:59 +0300

    seeder for events

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->text(30),
+            'start_date' => '2024-03-31',
+            'registration_until' => '2024-03-28',
+            'description' => fake()->text(),
+            'eventtype_id' => rand(1,4)
+        ];
+    }
+}
