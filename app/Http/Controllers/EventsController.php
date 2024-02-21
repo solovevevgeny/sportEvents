1cfa20cbbb73127c718109fb5a320408bee8c824
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-02-21 10:33:29 +0300

    list all events

---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Event;
+
+
+class EventsController extends Controller
+{
+    public function index(){
+        $events = Event::with('location','EventType')->orderBy('start_date')->get();
+        return response($events, 200);
+
+    }
+}
