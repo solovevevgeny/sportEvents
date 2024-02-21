ad7844663d98738ebc7b61109e6835036f1327ae
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-02-21 09:59:26 +0300

    realation hasOne to eventType

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\EventType;
+
 class Event extends Model
 {
     use HasFactory;
+
+    public function EventType(){
+        return $this->hasOne(EventType::class,'id','eventtype_id');
+    }
+
 }
