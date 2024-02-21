52caeb29b73cc45fc0bf101dd99107c799759493
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-02-21 10:13:45 +0300

    relation event->location

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\EventType;
+use App\Models\Location;
 
 class Event extends Model
 {
@@ -13,6 +14,10 @@ class Event extends Model
 
     public function EventType(){
         return $this->hasOne(EventType::class,'id','eventtype_id');
+    }
+
+    public function location(){
+        return $this->hasOne(Location::class, 'id', 'location_id');
     }
 
 }
